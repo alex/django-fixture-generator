07aa6832f9e62e4edc61762c22e4171d5e7cf68f
Alex Gaynor <alex.gaynor@gmail.com>
2010-06-11 22:24:51 -0500

    setup.py

--- fixture_generator/__init__.py
+++ fixture_generator/__init__.py
@@ -1,1 +1,3 @@
 from fixture_generator.fixture_gen import fixture_generator
+
+VERSION = (0, 1, 0)
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+
+from setuptools import setup, find_packages
+
+from fixture_generator import VERSION
+
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.txt'))
+readme = f.read()
+f.close()
+
+setup(
+    name='django-fixture-generator',
+    version=".".join(map(str, VERSION)),
+    description='django-fixutre-generator is a reusable django application to make writing fixtures not suck.',
+    long_description=readme,
+    author='Alex Gaynor',
+    author_email='alex.gaynor@gmail.com',
+    url='http://github.com/alex/django-fixture-generator/tree/master',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    test_suite='runtests.runtests'
+)
+
