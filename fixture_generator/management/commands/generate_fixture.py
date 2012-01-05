884c29a3c79180e53461639d94fb5796d81b88ac
Alex Gaynor <alex.gaynor@gmail.com>
2012-01-05 15:19:15 -0600

    useful error checking

--- fixture_generator/management/commands/generate_fixture.py
+++ fixture_generator/management/commands/generate_fixture.py
@@ -1,7 +1,7 @@
 import os
 from optparse import make_option
 
-from django.core.management import BaseCommand, call_command
+from django.core.management import BaseCommand, call_command, CommandError
 from django.conf import settings
 from django.db import router, connections
 from django.utils.importlib import import_module
@@ -43,16 +43,16 @@ def linearize_requirements(available_fixtures, fixture, seen=None):
 class FixtureRouter(object):
     def __init__(self, models):
         self.models = models
-    
+
     def db_for_read(self, model, instance=None, **hints):
         return FIXTURE_DATABASE
-    
+
     def db_for_write(self, model, instance=None, **hints):
         return FIXTURE_DATABASE
-    
+
     def allow_relation(self, *args, **kwargs):
         return True
-    
+
     def allow_syncdb(self, db, model):
         return True
 
@@ -65,7 +65,7 @@ class Command(BaseCommand):
             help="Specifies the indent level to use when pretty-printing output"),
     )
     args = "app_label.fixture"
-    
+
     def handle(self, fixture, **options):
         available_fixtures = {}
         for app in settings.INSTALLED_APPS:
@@ -79,10 +79,18 @@ class Command(BaseCommand):
                 if getattr(obj, "__fixture_gen__", False):
                     available_fixtures[(app.rsplit(".", 1)[-1], obj.__name__)] = obj
         app_label, fixture_name = fixture.rsplit(".", 1)
-        fixture = available_fixtures[(app_label, fixture_name)]
-        
+        try:
+            fixture = available_fixtures[(app_label, fixture_name)]
+        except KeyError:
+            available = ", ".join(
+                "%s.%s" % (app_label, fixture_name)
+                for app_label, fixture_name in available_fixtures
+            )
+            raise CommandError("Fixture generator '%s' not found, available "
+                "choices: %s" % (fixture, available))
+
         requirements, models = linearize_requirements(available_fixtures, fixture)
-        
+
         settings.DATABASES[FIXTURE_DATABASE] = {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": "fixture_gen.db",
--- fixture_generator/tests/tests.py
+++ fixture_generator/tests/tests.py
@@ -46,46 +46,63 @@ class LinearizeRequirementsTests(TestCase):
         ]
         for fixture in fixtures:
             self.available_fixtures[("tests", fixture)] = globals()[fixture]
-    
+
     def linearize_requirements(self, test_func):
         return linearize_requirements(self.available_fixtures, test_func)
-    
+
     def test_basic(self):
         requirements, models = self.linearize_requirements(test_func_1)
         self.assertEqual(requirements, [test_func_1])
         self.assertEqual(models, [])
-    
+
     def test_diamond(self):
         requirements, models = self.linearize_requirements(test_func_2)
         self.assertEqual(
             requirements,
             [test_func_5, test_func_3, test_func_4, test_func_2]
         )
-    
+
     def test_circular(self):
         self.assertRaises(CircularDependencyError,
             linearize_requirements, self.available_fixtures, test_func_6
         )
 
 
+class NullContextManager(object):
+    def __enter__(self):
+        pass
+    def __exit__(self, *args):
+        pass
+
 class ManagementCommandTests(TestCase):
-    def generate_fixture(self, fixture):
-        out = sys.stdout
-        sys.stdout = StringIO()
-        try:
-            call_command("generate_fixture", fixture)
-            output = sys.stdout.getvalue()
-        finally:
-            sys.stdout = out
-        return output
-    
+    def generate_fixture(self, fixture, error=False):
+        stdout = StringIO()
+        stderr = StringIO()
+        if error:
+            cm = self.assertRaises(SystemExit)
+        else:
+            cm = NullContextManager()
+        with cm:
+            call_command("generate_fixture", fixture, stdout=stdout, stderr=stderr)
+        return stdout.getvalue(), stderr.getvalue()
+
     def test_basic(self):
-        output = self.generate_fixture("tests.test_1")
+        output, _ = self.generate_fixture("tests.test_1")
         self.assertEqual(output, """[{"pk": 1, "model": "tests.author", "fields": {"name": "Tom Clancy"}}, {"pk": 2, "model": "tests.author", "fields": {"name": "Daniel Pinkwater"}}]""")
-    
+
     def test_auth(self):
         # All that we're checking for is that it doesn't hang on this call,
         # which would happen if the auth post syncdb hook goes and prompts the
         # user to create an account.
-        output = self.generate_fixture("tests.test_2")
+        output, _ = self.generate_fixture("tests.test_2")
         self.assertEqual(output, "[]")
+
+    def test_nonexistant(self):
+        out, err = self.generate_fixture("tests.test_255", error=True)
+        # Has a leading shell color thingy.
+        self.assertTrue("Error: Fixture generator 'tests.test_255' not found" in err)
+        self.assertFalse(out)
+
+        out, err = self.generate_fixture("xxx.xxx", error=True)
+        self.assertTrue("Error: Fixture generator 'xxx.xxx' not found" in err)
+        self.assertFalse(out)
